dcbda7092856c52ac6c85a94c8064c51dc334acb
Matt Pladna <pladdypants@gmail.com>
2019-02-18 21:15:22 -0500

    add codecov; edit travis file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ require "bundler/setup"
 require 'simplecov'
 SimpleCov.start
 
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 require "algorithms"
 require "data_structures"
 
